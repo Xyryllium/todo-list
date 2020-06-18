8e261b75f336b124e68ba313fe6fa6b7722826d6
Xy <50977871+Xyryllium@users.noreply.github.com>
2020-06-18 15:21:28 +0800

    Updates

---
+++ precache-manifest.dda23168f78b6f67d25f1e071e864aed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e9d4b6cddfabaade300e5a7bff1f07",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "3a9834e522b81b279266",
+    "url": "/todo-list/static/css/main.4104e3a5.chunk.css"
+  },
+  {
+    "revision": "63978f6de225adcf3fba",
+    "url": "/todo-list/static/js/2.a5a14d27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list/static/js/2.a5a14d27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9834e522b81b279266",
+    "url": "/todo-list/static/js/main.92bfb157.chunk.js"
+  },
+  {
+    "revision": "2f2dbf2866352368f122",
+    "url": "/todo-list/static/js/runtime-main.cc457382.js"
+  }
+]);
